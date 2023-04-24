b904b01c2337d057fa3533e034741d30a161be5a
summe <summe@LAPTOP-OULMJUCH>
2023-04-24 16:11:30 +0900

    02_01_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -4,16 +4,31 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete model_;
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+	textureHandle_ = TextureManager::Load("debugfont.png");
+
+	//モデル
+	viewProjection_.Initialize();
+	model_ = Model::Create();
+
+	//自キャラ
+	player_ = new Player();
+	player_->Initialize(model_,textureHandle_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+	player_->Update(); 
+}
 
 void GameScene::Draw() {
 
@@ -40,6 +55,8 @@ void GameScene::Draw() {
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
+	player_->Draw(viewProjection_);
+	
 	/// </summary>
 
 	// 3Dオブジェクト描画後処理
